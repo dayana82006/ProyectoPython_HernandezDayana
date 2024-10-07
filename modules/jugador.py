dfd696b822c9d4e06e18ceff88529a416e2346b1
dayana <123024360+dayana82006@users.noreply.github.com.>
2024-10-06 23:49:41 -0500

    jjjjj

--- modules/jugador.py
+++ modules/jugador.py
@@ -1,64 +1,56 @@
-import modules as mdl
+import modules as mdls
 import os
 import sys
 
 def jugarJug():
-    import os 
-import sys
-import modules as mdls
-import random
-#from tabulate import tabulate
-
-contador={
-    'PuntosPc':0,
-    'PuntosUs':0,
-    'rondasGU':0,
-    'rondasGM':0,
-    'resultados':0
-}
-def jugarPc():
+    contador={
+        'PuntosJ1':0,
+        'PuntosJ2':0,
+        'rondasJ1':0,
+        'rondasJ2':0,
+        'resultados':0
+    }
 
     isActive=True
     user=str(input('🃟 Registre su nombre de usuario1: '))
     user2=str(input('🃟 Registre su nombre de usuario2: '))
     users={
-            'name':user
+            'name':user,
+            'name2':user2
             }
-    print('USTED JUGARA CONTRA EL PC')
+    
     while isActive:
-        maquina=['piedra', 'papel', 'tijera']
-        if contador['rondasGU']<3 and contador['rondasGM']<3:
+        opciones=['piedra', 'papel', 'tijera']
+        if contador['rondasJ1']<3 and contador['rondasJ2']<3:
             print(mdls.opcPC)
-            opcUs=(input('ELije sabiamente: ')).lower()
-            opcionPc=random.choice(maquina)
-            if opcUs=='piedra' and opcionPc=='tijera':
-                print('Eso es, Has ganado')
-                contador['rondasGU']+=1
-                print(f'MARCADOR {contador.get('PuntosPC')} - {contador.get('PuntosUs')}')
-
-            elif opcUs=='papel' and opcionPc=='piedra':
-                print('Bien Has ganado')
-                contador['rondasGU']+=1
-                print(f'MARCADOR {contador.get('PuntosPC')} - {contador.get('PuntosUs')}')
-            elif opcUs=='tijera' and opcionPc=='papel':
-                    print('Ey!, Muy bien Has ganado')
-                    contador['rondasGU']+=1
-                    print(f'MARCADOR {contador.get('PuntosPC')} - {contador.get('PuntosUs')}')
-            elif opcionPc=='piedra' and opcUs=='tijera':
-                print('Lo siento, no ganaste esta partida')
-                contador['rondasGM']+=1
-                print(f'MARCADOR {contador.get('PuntosPC')} - {contador.get('PuntosUs')}')
-            elif opcionPc=='papel' and opcUs=='piedra':
-                print('Que triste, no ganaste esta partida')
-                contador['rondasGM']+=1
-                print(f'MARCADOR {contador.get('PuntosPC')} - {contador.get('PuntosUs')}')
-            elif opcionPc=='tijera' and opcUs=='papel':
-                print('Bueno, no ganaste esta partida')
-                contador['rondasGM']+=1
-                print(f'MARCADOR {contador.get('PuntosPC')} - {contador.get('PuntosUs')}')
-            elif opcUs==opcionPc:
-                print('Es un empate... Aun puedes ganar!')   
-                print(f'MARCADOR {contador.get('PuntosPC')} - {contador.get('PuntosUs')}') 
+            opcJ1=(input('Jugador 1 elije: ')).lower()
+            opcJ2=(input('Jugador 2 elije: ')).lower()
+            if opcJ1=='piedra' and opcJ2=='tijera':
+                print(f'Va ganando {user}')
+                contador['rondasJ1']+=1
+                contador['PuntosJ1']+=2
+                print(f'MARCADOR {contador.get('PuntosJ2')} - {contador.get('PuntosJ1')}')
+            elif opcJ1=='papel' and opcJ2=='piedra':
+                print(f'{user} Ha ganado esta ronda')
+                contador['PuntosJ1']+=2
+                contador['rondasJ1']+=1
+                print(f'MARCADOR {contador.get('PuntosJ2')} - {contador.get('PuntosJ1')}')
+            elif opcJ1=='tijera' and opcJ2=='papel':
+                print(f'Va ganando {user}')
+                contador['rondasJ1']+=1
+                contador['PuntosJ1']+=2
+                print(f'MARCADOR {contador.get('PuntosJ2')} - {contador.get('PuntosJ1')}')
+            elif opcJ2=='piedra' and opcJ1=='tijera':
+                print(f'{user2} Ha ganadoe sta ronda')
+                contador['PuntosJ2']+=2
+                contador['rondasJ2']+=1
+                print(f'MARCADOR {contador.get('PuntosJ2')} - {contador.get('PuntosJ2')}')
+            elif opcJ2=='papel' and opcJ1=='piedra':
+                
+            elif opcJ2=='tijera' and opcJ1=='papel':
+               
+            elif opcJ1==opcJ2:
+                 
             else:
                 print('digite un dato correcto')
                 input('Presione cualquier tecla para volver a intentar...')
