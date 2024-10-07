ca8e4c0f4d6bb67464fc99b3141bcaf2dc6772d5
Dayana <dayana82006@gmail.com>
2024-10-07 16:53:02 -0500

    jjj

--- modules/jugador.py
+++ modules/jugador.py
@@ -10,9 +10,11 @@ def jugarJug(marcador):
         'rondasJ2':0,
         'resultados':0
     }
-
-    
-    nombreC=str(input('Ingrese su nombre completo'))
+    rondasGCJ1 = 0  
+    rondasGCJ2 = 0  
+    escudoJ1 = False
+    escudoJ2 = False
+    nombreC=str(input('🃟 Ingrese su nombre completo'))
     user=str(input('🃟 Registre su nombre de usuario1: '))
     user2=str(input('🃟 Registre su nombre de usuario2: '))
     users={  
@@ -32,39 +34,41 @@ def jugarJug(marcador):
             print(mdls.opcPC)
             opcJ1=(input('Jugador 1 elije: ')).lower()
             opcJ2=(input('Jugador 2 elije: ')).lower()
-            if opcJ1=='piedra' and opcJ2=='tijera':
+            if opcJ1=='piedra' and opcJ2=='tijera' or opcJ1=='papel' and opcJ2=='piedra' or opcJ1=='tijera' and opcJ2=='papel':
+                rondasGCJ1 += 1
+                rondasGCJ2 = 0
+                if rondasGCJ1 == 2 and not escudoJ1:
+                    escudoJ1 = True 
+                    print(f'{user} ha obtenido un escudo por dos victorias consecutivas!')
                 print(f'Va ganando {user}')
                 contador['rondasJ1']+=1
                 print(f'MARCADOR {contador["rondasJ2"]} - {contador["rondasJ1"]}')
-            elif opcJ1=='papel' and opcJ2=='piedra':
-                print(f'{user} Ha ganado esta ronda')
-                contador['rondasJ1']+=1
-                print(f'MARCADOR {contador["rondasJ2"]} - {contador["rondasJ1"]}')
-            elif opcJ1=='tijera' and opcJ2=='':
-                print(f'{user} Ha ganado esta ronda')
-                contador['rondasJ1']+=1
-                print(f'MARCADOR {contador["rondasJ2"]} - {contador["rondasJ1"]}')
-            elif opcJ2=='piedra' and opcJ1=='tijera':
-                print(f'{user2} Ha ganade esta ronda') 
-                contador['rondasJ2']+=1
+            elif opcJ2=='piedra' and opcJ1=='tijera' or opcJ2=='papel' and opcJ1=='piedra' or opcJ2=='tijera' and opcJ1=='papel':
+                rondasGCJ2 += 1
+                rondasGCJ1 = 0
+                if rondasGCJ2 == 2 and not escudoJ2:
+                    escudoJ2 = True
+                    print(f'{user2} ha obtenido un escudo por dos victorias consecutivas!')
+                if escudoJ1:
+                    print(f'{user} ha usado su escudo y se salva de la derrota!')
+                    escudoJ1 = False
+                else:
+                    print(f'{user2} Ha ganado esta ronda') 
+                    contador['rondasJ2']+=1
                 print(f'MARCADOR {contador.get("rondasJ2")} - {contador["rondasJ1"]}')
-            elif opcJ2=='papel' and opcJ1=='piedra':
-                print(f'{user2} Ha ganado esta ronda') 
-                contador['rondasJ2']+=1
-                print(f'MARCADOR {contador.get("rondasJ2")} - {contador["rondasJ1"]}')
-            elif opcJ2=='tijera' and opcJ1=='papel':
-               print(f'{user2} Ha ganado esta ronda') 
-               contador['rondasJ2']+=1
-               print(f'MARCADOR {contador.get("rondasJ2")} - {contador["rondasJ1"]}')
             elif opcJ1==opcJ2:
-                print('Esto es un empate, re reinicia marcador') 
-                contador["rondasJ2"]=0
-                contador["rondasJ1"]=0
-
+                print('Esto es un empate, se han acabado las rachas') 
+                rondasGCJ1 = 0
+                rondasGCJ2 = 0
                 print(f'MARCADOR {contador.get("rondasJ2")} - {contador["rondasJ1"]}')
             else:
                 print('digite un dato correcto')
                 input('Presione cualquier tecla para volver a intentar...')
+            
+            print(f'Victorias consecutivas de {user}: {rondasGCJ1}')
+            print(f'Victorias consecutivas de {user2}: {rondasGCJ2}')
+            print(f'Escudo de {user}: {"Activo" if escudoJ1 else "Inactivo"}')
+            print(f'Escudo de {user2}: {"Activo" if escudoJ2 else "Inactivo"}')
         else:
             print('La partida ha finalizado')
             if contador['rondasJ1'] > contador['rondasJ2']:
@@ -78,9 +82,6 @@ def jugarJug(marcador):
              print(f'✵°✵.｡.✰ FELICIDADES {users["nickname2"]} GANASTE !! ✰.｡.✵°✵')
              print(f'LO SIENTO {users["nickname"]} HAS PERDIDO ٩꒰´·⌢•｀꒱۶⁼³₌₃')
              mdls.pausar_pantalla()
-             return(jugarJug)
              isActive=False
              contador['rondasJ1']=0
              contador['rondasJ2']=0
-            
-       
